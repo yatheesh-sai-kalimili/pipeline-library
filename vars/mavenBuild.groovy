78c691b3c6bc7d14fd6aa83803d1148d891af596
Yatheesh Sai <yatheeshwtwd@gmail.com>
2023-02-25 20:46:23 +0530

    first commit

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -33,9 +33,6 @@ pipeline {
              equals(actual: args.buildType, expected: "Java build")
         }
         steps {
-            sh 'export MAVEN_HOME=/opt/maven'
-            sh 'export PATH=$PATH:$MAVEN_HOME/bin'
-            sh 'mvn --version'
             sh 'mvn clean package'
         }
         }
