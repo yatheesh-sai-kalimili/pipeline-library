fb750fe427a38cc479b296fe153d030a26156339
Yatheesh Sai <yatheeshwtwd@gmail.com>
2023-02-25 19:05:00 +0530

    first commit

---
+++ vars/mavenBuild.groovy
@@ -0,0 +1,38 @@
+#!/usr/bin/env groovy
+def call (body) {
+   def args = [
+       branch: '',
+       url: '',
+       service: ''
+  ]
+
+body.resolveStrategy = Closure.DELEGATE_FIRST
+body.delegate = args
+body ()
+echo "INFO: ${args.service}"
+echo "INFO: ${args.branch}"
+
+pipeline {
+   agent any
+   tools {
+    maven 'maven'
+    jdk 'jdk'
+    }
+   stages {
+      stage('Git Checkout') {
+        steps {
+           checkout([
+              $class: 'GitSCM',
+              branches: [[name: args.branch ]],
+              userRemoteConfigs: [[ url: args.url ]]
+              ])
+              }
+        }
+       stage ('Build') {
+        steps {
+           bat 'mvn -Dmaven.test.failure.ignore=true install'
+        }
+        }
+    }
+}
+}
